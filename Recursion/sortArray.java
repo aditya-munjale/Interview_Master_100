14ba4b60517e7df9d416794447ed766ca94ff5f5
Aditya Munjale <munjaleaditya13@gmail.com>
2025-09-11 17:45:55 +0530

    Sort an Array

--- Recursion/sortArray.java
+++ Recursion/sortArray.java
@@ -1,5 +1,40 @@
 package Recursion;
+import java.util.*;
 
 public class sortArray {
+
+    public static void sort(ArrayList<Integer> list){
+        if(list.size() == 1){
+            return;
+        }
+
+        int temp = list.get(list.size()-1);
+        list.remove(list.size()-1);
+        sort(list);
+        insert(temp,list);
+    }
+
+    public static void insert(int temp, ArrayList<Integer> list){
+        if(list.size() == 0 || list.get(list.size()-1) <= temp){
+            list.add(temp);
+            return;
+        }
+
+        int val = list.get(list.size()-1);
+        list.remove(list.size()-1);
+        insert(temp,list);
+        list.add(val);
+    }
+
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<>(Arrays.asList(9,2,4,6,1,5));
+
+        System.out.println(list);
+        sort(list);
+        
+        System.out.println(list);
+
+    
+    }
     
 }
